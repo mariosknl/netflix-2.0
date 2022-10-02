9fc7bfe248f0087c4e9aa45d782829ae74c6425f
mariosknl <mariosknl@outlook.com>
2022-10-02 07:57:30 +0300

    building the nav component

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyB3NPEHgytbNV9UjTKloFIA0RaVqK91emM",
+	authDomain: "netflix-v2-394b9.firebaseapp.com",
+	projectId: "netflix-v2-394b9",
+	storageBucket: "netflix-v2-394b9.appspot.com",
+	messagingSenderId: "808727795876",
+	appId: "1:808727795876:web:cc4c892f480bac30972e31",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
