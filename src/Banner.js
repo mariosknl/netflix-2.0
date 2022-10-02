49379ecf30b299aef06c34988c517a613b7fb9c4
mariosknl <mariosknl@outlook.com>
2022-10-02 10:27:07 +0300

    building the banner component 2/2

--- src/Banner.js
+++ src/Banner.js
@@ -31,21 +31,20 @@ const Banner = () => {
 			className="banner"
 			style={{
 				backgroundSize: "cover",
-				backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
 				backgroundPosition: "center center",
 			}}
 		>
 			<div className="banner__contents">
-				<h1 className="banner__title">Movie Name</h1>
+				<h1 className="banner__title">
+					{movie?.title || movie?.name || movie?.original_name}
+				</h1>
 				<div className="banner__buttons">
 					<button className="banner__button">Play</button>
 					<button className="banner__button">My List</button>
 				</div>
 				<h1 className="banner__description">
-					{truncate(
-						`This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`,
-						150
-					)}
+					{truncate(movie?.overview, 150)}
 				</h1>
 			</div>
 			<div className="banner--fadeBottom" />
