385c2081f99f396a92851cf4b6902d01ca7b39e5
mariosknl <mariosknl@outlook.com>
2022-10-09 07:46:32 +0300

    implementing the loginScreen component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { LoginScreen } from "./screens/LoginScreen";
 
 function App() {
+	const user = null;
+
 	return (
 		<div className="app">
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<HomeScreen />
-					</Route>
-				</Switch>
-			</Router>
+			{!user ? (
+				<LoginScreen />
+			) : (
+				<Router>
+					<Switch>
+						<Route exact path="/">
+							<HomeScreen />
+						</Route>
+					</Switch>
+				</Router>
+			)}
 		</div>
 	);
 }
